654528309402dbc9bd337cbe6f01ab8ee7bc2960
Kouhei Sano <ratatatta6@gmail.com>
2018-01-13 22:01:41 +0900

    add comments

--- app/src/main/java/com/example/kouhei/tabapplication/MainActivity.java
+++ app/src/main/java/com/example/kouhei/tabapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends FragmentActivity {
     private FragmentTabHost mTabHost;
@@ -17,14 +18,20 @@ public class MainActivity extends FragmentActivity {
         mTabHost.setup(this, getSupportFragmentManager(), R.id.tabcontent);
 
         // "FragmenteTab"は自分で実装するクラス
+        // addTab()の第一引数がTabSpec、第二引数は表示するFragmentをclass名で渡す。第三引数はFragmentで読み込むデータをBundleで渡す。
+        // TabSpecはタブの情報
+        // setIndicator()はタブのView
         mTabHost.addTab(
                 mTabHost.newTabSpec("tab1").setIndicator("Tab 1", null),
-                FragmentTab.class, null);
+                FragmentTab.class,
+                null);
         mTabHost.addTab(
                 mTabHost.newTabSpec("tab2").setIndicator("Tab 2", null),
-                FragmentTab.class, null);
+                FragmentTab.class,
+                null);
         mTabHost.addTab(
                 mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),
-                FragmentTab.class, null);
+                FragmentTab.class,
+                null);
     }
 }
